e3addedea70c359cf71acb856e68f498d89897c5
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-06-22 11:53:38 +0100

    deployment testing07

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -4,7 +4,7 @@ import {} from "wix-style-react";
 export interface AppProps {}
 
 const App: React.SFC<AppProps> = (): JSX.Element => {
-  return <div>Hello Wolrd, I am from Heroku</div>;
+  return <div>Hello Wolrd, I am from Heroku, Helo</div>;
 };
 
 export default App;
