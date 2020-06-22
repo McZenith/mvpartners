2de54b9c0fcbb5dc4c34fd8cdd27379ab0a8b924
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-06-22 11:35:48 +0100

    deployment testing04

--- client/dev/server.js
+++ client/dev/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const session = require('express-session');
-const renderVM = require('./vm');
+const express = require("express");
+const session = require("express-session");
+const renderVM = require("./vm");
 
 const app = express();
 
 // Register an express middleware. Learn more: http://expressjs.com/en/guide/using-middleware.html.
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
-    saveUninitialized: true,
-  }),
+    saveUninitialized: true
+  })
 );
 
 // Define a route to render our initial HTML.
-app.use('/', (req, res) => {
+app.use("/", (req, res) => {
   if (!req.session.visitCount) {
     req.session.visitCount = 0;
   }
@@ -22,13 +22,14 @@ app.use('/', (req, res) => {
   req.session.visitCount++;
 
   const html = renderVM({
-    visitCount: req.session.visitCount,
+    visitCount: req.session.visitCount
   });
 
   res.send(html);
 });
 
+const PORT = process.env.PORT || 3000;
 // Launch the server
-app.listen(process.env.PORT, () => {
-  console.info(`Fake server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.info(`Fake server is running on port ${PORT}`);
 });
